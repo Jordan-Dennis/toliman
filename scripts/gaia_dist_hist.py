190d6d3d880bf94a1316e83941eaddecaab6de7d
Jordan Dennis <jdenn105@gmail.com>
2022-11-24 10:49:17 +1100

    So it appears that the Gaia data is only missing a month or so of stars that were obscurred by Alpha Centauri. I'm not convinced that the signs make sense as it is at the momement.

--- scripts/gaia_dist_hist.py
+++ scripts/gaia_dist_hist.py
@@ -20,10 +20,10 @@ alpha_cen_ra = 219.902540961
 alpha_cen_dec = -60.8330381775
 window_width = 6. / 60.
 radius = 2. / 60.
-alpha_cen_prop_mot_ra = -7.861312663448992
-alpha_cen_prop_mot_dec = -4.2193571220003445
-alpha_cen_delta_ra = alpha_cen_prop_mot_ra * 8. / 3600.
-alpha_cen_delta_dec = alpha_cen_prop_mot_dec * 8. / 3600.
+alpha_cen_prop_mot_ra = -3608
+alpha_cen_prop_mot_dec = 686
+alpha_cen_delta_ra = - alpha_cen_prop_mot_ra * .05 / 3600.
+alpha_cen_delta_dec = - alpha_cen_prop_mot_dec * .05 / 3600.
 alpha_cen_orig_ra = alpha_cen_ra - alpha_cen_delta_ra
 alpha_cen_orig_dec = alpha_cen_dec - alpha_cen_delta_dec
 
@@ -44,7 +44,7 @@ axes = plt.axes()
 axes.scatter(ra, dec, alpha=alpha + 1.)
 axes.add_patch(patch.Circle((alpha_cen_ra, alpha_cen_dec), radius=0.01, color="red"))
 axes.set_aspect(1 / axes.get_data_ratio())
-axes.arrow(alpha_cen_orig_ra, alpha_cen_orig_dec, alpha_cen_delta_ra, alpha_cen_delta_dec)
+axes.arrow(alpha_cen_orig_ra, alpha_cen_orig_dec, alpha_cen_delta_ra, alpha_cen_delta_dec, color="pink")
 
 plt.show()
 exit()
