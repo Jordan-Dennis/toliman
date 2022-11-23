557e7c38026a363cbd229fa7a7370946bee1974a
Jordan Dennis <jdenn105@gmail.com>
2022-11-23 13:30:49 +1100

    Improving the query by multiprocessing for performance

--- scripts/gaia_sky_survey.py
+++ scripts/gaia_sky_survey.py
@@ -1,9 +1,13 @@
 from astroquery.gaia import Gaia
+from multiprocessing import Pool 
 import jax.numpy as np
+import matplotlib as mpl 
+import matplotlib.pyplot as plt
+import itertools as it
 
 conical_query = """
 SELECT
-    ra, dec, phot_g_mean_mag AS mag
+    phot_g_mean_mag AS mag
 FROM
     gaiadr3.gaia_source
 WHERE
@@ -14,10 +18,19 @@ alpha_cen_ra = 219.902540961
 alpha_cen_dec = -60.8330381775
 window_width = 2. / 60.
 
-test = Gaia.launch_job(
-    conical_query.format(alpha_cen_ra, alpha_cen_dec, window_width))
-print(dir(test))
-print(test.results)
-print(type(test.results))
-print(dir(test.results))
-print(np.array(test.results["mag"]))
+figure, axes = plt.subplots(5, 5, sharex=True, sharey=True)
+
+def plot_tile_hist(ra: int, dec: int) -> None:
+    ra_centre = alpha_cen_ra + ra * window_width
+    dec_centre = alpha_cen_dec + dec * window_width
+    region = Gaia.launch_job(conical_query.format(
+        ra_centre, dec_centre, window_width))
+    return np.array(region.results["mag"])
+
+with Pool(16) as pool:
+    centres = it.product(range(-2, 3), range(-2, 3))
+    mags = pool.starmap(plot_tile_hist, centres)
+
+for ra in range(-2, 3):
+    for dec in range(-2, 3):
+        
