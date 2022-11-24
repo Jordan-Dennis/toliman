dbd6a628b52e2b239c5c26c46d7419c96c13274a
Jordan Dennis <jdenn105@gmail.com>
2022-11-24 14:35:07 +1100

    Either Alpha Centauri is not very bright in the G band or the Gaia data was saturated.

--- scripts/gaia_bg_sample.py
+++ scripts/gaia_bg_sample.py
@@ -18,6 +18,7 @@ WHERE
 
 alpha_cen_ra = 219.902540961
 alpha_cen_dec = -60.8330381775
+alpha_cen_mag = 18.03996 
 bg_ra = alpha_cen_ra + 0.1
 bg_dec = alpha_cen_dec
 bg_win = 2. / 60. 
@@ -40,13 +41,22 @@ bg_stars_mag_crop = bg_stars_mag[in_range]
 bg_stars_mag_no_nan_crop = np.where(np.isnan(bg_stars_mag_crop), np.nanmax(bg_stars_mag_crop), bg_stars_mag_crop)
 alpha_crop = (bg_stars_mag_no_nan_crop - bg_stars_mag_no_nan_crop.min()) / bg_stars_mag_no_nan_crop.ptp()
 
-fig = plt.figure(constrained_layout=True)
-axes = fig.add_gridspec(top=0.75, right=0.75).subplots()
+fig_1 = plt.figure(constrained_layout=True)
+axes = fig_1.add_gridspec(top=0.75, right=0.75).subplots()
 axes.set(aspect=1)
 axes_hist_x = axes.inset_axes([0, 1.05, 1, 0.25])
 axes_hist_y = axes.inset_axes([1.05, 0, 0.25, 1])
-axes.scatter(bg_stars_ra_crop, bg_stars_dec_crop, alpha=alpha)
+axes.scatter(bg_stars_ra_crop, bg_stars_dec_crop, alpha=alpha_crop)
 axes_hist_x.hist(bg_stars_ra_crop)
 axes_hist_y.hist(bg_stars_dec_crop, orientation="horizontal")
 plt.show()
 
+fig_2 = plt.figure()
+axes = plt.axes()
+fig_2.add_axes(axes)
+axes.hist(bg_stars_mag_crop)
+axes.axvline(alpha_cen_mag, color="red")
+axes.set_xlabel("phot_g_mean_mag")
+axes.set_ylabel("count")
+axes.set_title("Histogram of Magnitudes in the G Band")
+plt.show()
