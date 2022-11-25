4acf331ad040632a4a6ad13ed237ecaa72c0b7d2
Jordan Dennis <jdenn105@gmail.com>
2022-11-25 11:19:42 +1100

    Completing the tests for the `CircularAperture` apertures, this will serve as a template for the other apertures.

--- scripts/dlux_aperture_tests.py
+++ scripts/dlux_aperture_tests.py
@@ -4,8 +4,13 @@ import dLux as dl
 
 mpl.rcParams["text.usetex"] = True
 mpl.rcParams["image.cmap"] = "inferno"
+mpl.rcParams["axes.spines.top"] = False
+mpl.rcParams["axes.spines.left"] = False
+mpl.rcParams["axes.spines.right"] = False
+mpl.rcParams["axes.spines.bottom"] = False
 
-coordinates = dl.utils.get_pixel_coordinates(256, 1. / 256.)
+
+coordinates = dl.utils.get_pixel_coordinates(24, 2. / 24.)
 
 # My philosphy with the following trest cases is that each feature should
 # only be tested once. In other words, I do not need to test every combination 
@@ -20,11 +25,53 @@ pos_y_circ = dl.CircularAperture(0., 1., 1., occulting=True, softening=True)
 pos_x_circ = dl.CircularAperture(1., 0., 1., occulting=True, softening=True)
 neg_y_circ = dl.CircularAperture(0., -1., 1., occulting=True, softening=True)
 neg_x_circ = dl.CircularAperture(-1., 0., 1., occulting=True, softening=True)
-neg_rad_circ = dl.CircularAperture(0., 0., -1., occulting=True, softening=True)
 
-fig = plt.figure()
+fig, axes = plt.subplots(3, 3)
+fig.suptitle("Circular Aperture Tests")
+axes[0][0].set_title("Occulting, Soft Edged")
+_map = axes[0][0].imshow(occ_soft_circ._aperture(coordinates))
+axes[0][0].set_xticks([])
+axes[0][0].set_yticks([])
+fig.colorbar(_map)
+axes[0][1].set_title("Occulting, Hard Edged")
+_map = axes[0][1].imshow(occ_circ._aperture(coordinates))
+axes[0][1].set_xticks([])
+axes[0][1].set_yticks([])
+fig.colorbar(_map)
+axes[0][2].set_title("Not Occulting, Soft Edged")
+_map = axes[0][2].imshow(soft_circ._aperture(coordinates))
+axes[0][2].set_xticks([])
+axes[0][2].set_yticks([])
+fig.colorbar(_map)
+axes[1][0].set_title("Not Occulting, Hard Edged")
+_map = axes[1][0].imshow(circ._aperture(coordinates))
+axes[1][0].set_xticks([])
+axes[1][0].set_yticks([])
+fig.colorbar(_map)
+axes[1][1].set_title("Positive $y$ Translation (Occ., Soft.)")
+_map = axes[1][1].imshow(pos_y_circ._aperture(coordinates))
+axes[1][1].set_xticks([])
+axes[1][1].set_yticks([])
+fig.colorbar(_map)
+axes[1][2].set_title("Positive $x$ Translation (Occ., Soft.)")
+_map = axes[1][2].imshow(pos_x_circ._aperture(coordinates))
+axes[1][2].set_xticks([])
+axes[1][2].set_yticks([])
+fig.colorbar(_map)
+axes[2][0].set_title("Negative $y$ Translation (Occ., Soft.)")
+_map = axes[2][0].imshow(neg_y_circ._aperture(coordinates))
+axes[2][0].set_xticks([])
+axes[2][0].set_yticks([])
+fig.colorbar(_map)
+axes[2][1].set_title("Negative $x$ Translation (Occ., Soft.)")
+_map = axes[2][1].imshow(neg_x_circ._aperture(coordinates))
+axes[2][1].set_xticks([])
+axes[2][1].set_yticks([])
+fig.colorbar(_map)
+axes[2][2].set_xticks([])
+axes[2][2].set_yticks([])
+plt.show()
 
-plt.imshow(occ_soft_circ._aperture(coordinates))
 ann_aper = dl.AnnularAperture()
 rect_aper = dl.RectangularAperture()
 sq_aper = dl.SquareAperture()
